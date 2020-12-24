342767eceb0fbf57e2a2d9d8f514bd7b69fa9654
deepasivapragasam <deepasivapragasam89@gmail.com>
2020-12-24 17:02:16 +0530

    changes in form

--- src/script/fair.js
+++ src/script/fair.js
@@ -1,52 +1,65 @@
 let next = document.getElementsByClassName("next");
 let back = document.getElementsByClassName("back");
-let dropBox= document.getElementsByClassName("drop-box")[0];
+let dropBox = document.getElementsByClassName("drop-box")[0];
+let sections= document.getElementsByTagName("section");
 
-for(let i=0;i<next.length;i++)
-{
-  next[i].addEventListener("click",function(){
-   this.parentNode.parentNode.parentNode.style.display="none";
-    this.parentNode.parentNode.parentNode.nextElementSibling.style.display="block";
-    console.log( this.parentNode.parentNode.parentNode.nextElementSibling);
+for (let i = 0; i < next.length; i++) {
+    next[i].addEventListener("click", function () {
+    sections[i].style.display = "none";
+    sections[i+1].style.display = "block";
+  });
+}
+for (let j = 0; j < back.length; j++) {
+  back[j].addEventListener("click", function () {
+  sections[j+1].style.display = "none";
+  sections[j].style.display = "block";
+  console.log(j);
+});
+}
+dropBox.addEventListener("click", function () {
+  console.log(this.target);
+  if (document.getElementsByClassName("drop-options")[0].style.display == "none") {
+    document.getElementsByClassName("drop-options")[0].style.display = "block";
+  }
+  else {
+    document.getElementsByClassName("drop-options")[0].style.display = "none";
+  }
+});
+
+let list = document.getElementsByClassName("list");
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    document.getElementsByClassName("drop-box")[0].innerHTML = this.innerHTML;
   });
 }
 
-for(let j=0;j<back.length;j++)
-{
-    back[j].addEventListener("click",function(){
-        this.parentNode.parentNode.parentNode.style.display="none";
-        this.parentNode.parentNode.parentNode.previousElementSibling.style.display="block";
+//range slider
+let slider = document.getElementsByClassName("slider");
+let selector = document.getElementsByClassName("selector");
+let progress = document.getElementsByClassName("progressBar");
+let output = document.getElementsByClassName("output");
+for (let i = 0; i < slider.length; i++) {
+  slider[i].addEventListener("input", function () {
+    selector[i].style.left = this.value + "%";
+    progress[i].style.width = this.value + "%";
+    output[i].value = "$" + this.value;
+    console.log(selector[i]);
+  });
+}
+
+//chekboxes
+let checkbox = document.querySelectorAll('.check');
+let checkBorder = document.querySelectorAll('.addBorder');
+for (let i = 0; i < checkbox.length; i++) {
+  console.log(checkbox[i]);
+  checkbox[i].addEventListener("click", function () {
+    if (checkbox[i].checked) {
+      checkBorder[i].classList.add("blue-border");
     }
-    );
+    else {
+      checkBorder[i].classList.remove("blue-border");
+    }
+  });
+
 }
 
-dropBox.addEventListener("click",function(){
-  console.log(this.target);
-  if(document.getElementsByClassName("drop-options")[0].style.display=="none")
-  {
-    document.getElementsByClassName("drop-options")[0].style.display="block";
-  }
-  else
-  {
-    document.getElementsByClassName("drop-options")[0].style.display="none";
-  }
-});
-
-let list=document.querySelectorAll(".list");
- for(var i=0;i<list.length;i++)
- {
-list[i].addEventListener("click",function(){ document.querySelector(".drop-box").value=this.innerHTML;
-});
- }
-
- //range slider
- var slider=document.querySelector(".slider");
-var selector=document.querySelector(".selector");
-var progress=document.querySelector(".progressBar");
-var output=document.querySelector(".output");
-slider.oninput=function()
-{
-  selector.style.left=this.value+"%";
-  progress.style.width=this.value+"%";
-  output.value="$"+this.value;
-}
